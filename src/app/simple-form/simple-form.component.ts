a78a97fee036111a72b14957a35fe908fb1eef57
taka334oboe <vaiomejiro@gmail.com>
2019-08-26 15:52:30 +0900

    最初のコミット

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  templateUrl: './simple-form.component.html',
+  styleUrls: ['./simple-form.component.scss']
+})
+export class SimpleFormComponent implements OnInit {
+  result:string=" 足し算をしましょう ";
+  text1:string;
+  text2:string;
+
+  addAndShow():void {
+    let forResult:string=" 正しい値を入力してください "
+    let int1:number;
+    let int2:number;
+
+    int1 = Number(this.text1);
+    int2 = Number(this.text2);
+
+    if(!Number.isNaN(int1) && !Number.isNaN(int2)){
+         forResult = `${int1}+${int2}=${int1+int2}`;
+    }
+     this.result=forResult;
+   }
+
+  constructor(){ }
+  
+   ngOnInit() {
+  }
+
+}
